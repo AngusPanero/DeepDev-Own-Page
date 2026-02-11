fa7405a0154857e17054715bc68305c5f7c67be0
Angus Panero <aguspanero@gmail.com>
2026-02-11 18:42:38 -0300

    ENV FIREBASE

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9M2ys-zsV6ahjzsCrFyK72vt78ZWru7o",
-  authDomain: "deepdev-own-page.firebaseapp.com",
-  projectId: "deepdev-own-page",
-  storageBucket: "deepdev-own-page.firebasestorage.app",
-  messagingSenderId: "923502643597",
-  appId: "1:923502643597:web:3ebbc39101c76dafcf55c4",
-  measurementId: "G-K7ES4103Q6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);

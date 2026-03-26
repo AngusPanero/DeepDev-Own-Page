a1cb860479efdddf2db456fa3090db9af049d198
Angus Panero <aguspanero@gmail.com>
2026-03-26 20:40:29 -0300

    FIX 2

--- src/components/sections/SalesHistory.tsx
+++ src/components/sections/SalesHistory.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useMemo, useEffect } from 'react';
-import '../../styles/salesHistory.css';
 import { UseTheme } from '../../contexts/ThemeContext';
+import "../../styles/salesHistory.css"
 
 interface Sale {
     _id: string;
